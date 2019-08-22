f073532165b81655c663b22a826d5fdfb4d7fae2
Christian <c.bock@stic-online.de>
2019-08-22 15:51:03 +0200

    add text box

--- src/features/draw.js
+++ src/features/draw.js
@@ -5,6 +5,7 @@ import Bezier from '../objects/bezier.js';
 import Circle from '../objects/circle.js';
 import Rectangle from '../objects/rectangle.js';
 import Pic from '../objects/pic.js';
+import Text from '../objects/text.js';
 
 import Event from "../helpers/event.js";
 
@@ -93,7 +94,7 @@ var Draw = class Draw {
         return this;
     }
 
-    hover(cursor, callback) {
+    hover(cursor, callback, config) {
         let e = new Event(this._current._id, 'mousemove', callback);
 
         e.shape = this._current;
@@ -106,6 +107,7 @@ var Draw = class Draw {
         }
 
         e.cursor = cursor;
+        e.threeSixty = config;
 
         /**
          * var EventStore window.eventStore
@@ -291,6 +293,85 @@ var Draw = class Draw {
         return this;
     }
 
+    text(charArray, x, y, color, size, font) {
+        let text = new Text(this.ctx(), charArray);
+
+        if(typeof x === 'undefined') {
+
+            x = 0;
+        }
+
+        if(typeof y === 'undefined') {
+
+            y = 0;
+        }
+
+        text
+            .x(x)
+            .y(y);
+
+        if(typeof color !== 'undefined') {
+
+            text.color(color);
+        }
+
+        if(typeof size !== 'undefined') {
+
+            text.size(size);
+        }
+
+
+        if(typeof font !== 'undefined') {
+
+            text.font(font);
+        }
+
+        text.draw();
+
+        return this;
+    }
+
+    textBox(charArray, x, y, textConfig, boxConfig) {
+        let padding = {
+                x: 0,
+                y: 0
+            },
+            metrics = this.textMetrics(charArray, textConfig.font, textConfig.size);
+
+        if(typeof textConfig === 'undefined') {
+
+            textConfig = {};
+        }
+
+        if(boxConfig.padding) {
+
+            if(boxConfig.padding.x) {
+
+                padding.x = boxConfig.padding.x;
+            }
+
+            if(boxConfig.padding.y) {
+
+                padding.y = boxConfig.padding.y;
+            }
+        }
+
+        this
+            .begin()
+            .color(boxConfig.color)
+            .rectangle(
+                x,
+                y,
+                metrics.width + 2*padding.x,
+                metrics.height + 2*padding.y
+            )
+            .close();
+
+        this.text(charArray, x + padding.x, y + textConfig.size + 0*padding.y, textConfig.color, textConfig.size, textConfig.font);
+
+        return this;
+    }
+
     collection() {
 
         return this._collection;
@@ -312,6 +393,22 @@ var Draw = class Draw {
         }
 
         return shape;
+    }
+
+    textMetrics(text, font, size) {
+        let canvas = this.textMetrics.canvas || (this.textMetrics.canvas = document.createElement("canvas")),
+            context = canvas.getContext('2d');
+
+        canvas.classList.add('d-none');
+
+        context.font = `${size}px ${font}`;
+
+        let metrics = context.measureText(text);
+
+        return {
+            width: metrics.width,
+            height: size
+        }
     }
 };
 
